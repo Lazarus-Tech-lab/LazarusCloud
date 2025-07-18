f299ffc3475d0deb519fb0bb037772c1250452a1
redgroul <redgroulman@gmail.com>
2025-07-18 20:28:35 +0300

    [FRONTEND] добавление skeleton и загрузка хранилища

---
+++ src/main/java/ru/red/lazaruscloud/controller/UserController.java
@@ -0,0 +1,27 @@
+package ru.red.lazaruscloud.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ru.red.lazaruscloud.dto.cloudDtos.UserDataDto;
+import ru.red.lazaruscloud.model.LazarusUserDetail;
+import ru.red.lazaruscloud.service.CloudQuotaService;
+import ru.red.lazaruscloud.service.UserStorageService;
+
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
+public class UserController {
+
+    private final UserStorageService userStorageService;
+
+    @GetMapping("/userdata")
+    public ResponseEntity<UserDataDto> getUserData(@AuthenticationPrincipal LazarusUserDetail userDetail) {
+        return new ResponseEntity<>(userStorageService.getUserData(userDetail), HttpStatus.OK);
+    }
+}
---
+++ src/main/java/ru/red/lazaruscloud/dto/cloudDtos/UserDataDto.java
@@ -0,0 +1,3 @@
+package ru.red.lazaruscloud.dto.cloudDtos;
+
+public record UserDataDto(Long id, String username, long quota, long usedQuota) {}
